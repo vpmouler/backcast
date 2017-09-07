1e6e8b2f1d72c3a75b2776c9fe40a1e560b64569
vpmouler <vpmouler@gmail.com>
2017-09-06 20:06:13 -0700

    finished first set of tests, deleting old code

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,8 +1,33 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    // console.log('this .collection', this.collection)
+    // access videoListEntry's render function and invoke it
+      // console.log('hiasdfasdf', this.prototype.VideoListEntry)
+    this.collection.on('sync',this.render,this);
+  },
+
 
   render: function() {
     this.$el.children().detach();
+
+    // console.log(VideoListEntryView.render)
+    
+    // for (var i = 0; i < 5; i ++ ) {
+    //   window.VideoListEntryView.prototype.render();
+    // }
+    this.collection.each(function(ele, index) {
+      new VideoListEntryView({ model: ele }).render();
+    });
+    // _.each(this.collection, function(model) {
+    // //   console.log('within each');
+    //   console.log(model)
+    //   // new VideoListEntryView(model).render();
+    // //   console.log('inside render this',this);
+    // //   console.log('inside render this.proto',this.prototype)
+    // //   this.VideoListEntryView.prototype.render();
+    //     // window.VideoListEntryView.prototype.render();
+    // },this);
     this.$el.html(this.template());
     return this;
   },
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,7 +1,16 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  // initialize: function() {
+  //       _.each(this.collection, function(){
+  //       this.render()}, this);
+  // }
+  // initialize: function() {
+  //   this.render();
+  // },
+
 
   render: function() {
+    
     this.$el.html(this.template(this.model.attributes));
     return this;
   },
